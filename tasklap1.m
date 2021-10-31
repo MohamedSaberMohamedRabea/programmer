054b43a890f2d49ef56213453310c077aedf0a5c
MohamedSaberMohamedRabea <93198773+MohamedSaberMohamedRabea@users.noreply.github.com>
2021-10-31 16:27:42 +0200

    Add files via upload
    محمد صابر محمد ربيع   B4

---
+++ tasklap1.m
@@ -0,0 +1,49 @@
+%%task lap 1.
+%% program &1
+clc
+clear
+A=[-7 5 -9;2 -1 2;1 -1 2];
+B=[16 3 2 13;5 10 11 8;9 6 7 12;4 15 14 1];
+D=[6 3 2;2 12 -7;-1 6 2;-5 15 11];
+C=[4 2 -3;7 -7 9;3 -5 6];
+Z=(3*A)-(5*C);
+disp(Z)
+
+%%T=(7*A)-(2*B);error not size arrays...
+
+C*A
+
+%%C*D error 
+%% program &2
+clc
+clear
+N=3;
+Zeros (2)
+Zeros (2,3)
+Ones (3)
+Ones(2,3)
+Size(D)
+Zeros(size(D))
+diag([1 2 3 4] )
+eye(N)
+
+%% program &3 
+clc
+clear
+%%[A,B]the number rows B greater than rows A..
+%%[A;B]the number colums B greater than colums A..
+
+%% program 4
+clc
+clear
+M=diag ([5 5 5 5 5 5 5 5])
+M(:,8) =[5 5 5 5 5 5 5 5]
+
+%% program &1
+clc
+clear
+I=1;
+J=2;
+A(I,:)
+A(:,J)
+          %% Mohamed Saber Mohamed Rabea %%
